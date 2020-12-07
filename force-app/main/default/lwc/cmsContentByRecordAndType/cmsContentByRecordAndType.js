874f2895635bf58c6b607fb29c1cadc653e6ed24
ildiavolorosso <morse.joe@gmail.com>
2020-12-07 09:03:03 -0800

    Update cmsContentByRecordAndType.js

--- force-app/main/default/lwc/cmsContentByRecordAndType/cmsContentByRecordAndType.js
+++ force-app/main/default/lwc/cmsContentByRecordAndType/cmsContentByRecordAndType.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import { LightningElement, wire, api } from 'lwc';
 import basePath from '@salesforce/community/basePath';
 import getCMSContent from '@salesforce/apex/ManagedContentController.getCMSContent';
